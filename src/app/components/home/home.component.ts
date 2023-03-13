544072c009def4bc8ef1882ab7a8ae31af904a59
mostafa <mostafa36ahmed@gmail.com>
2023-03-13 05:13:35 +0200

    add product to cart

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Orders } from 'src/app/models/orders';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart/cart.service';
 import { OrderService } from 'src/app/services/order/order.service';
 import { ProductService } from 'src/app/services/product/product.service';
 
@@ -12,22 +13,25 @@ import { ProductService } from 'src/app/services/product/product.service';
 })
 export class HomeComponent implements  OnInit {
 
-  constructor(private productservice : ProductService, private router: Router ) { 
-  }
   products:Product[]=[];
+  
+  constructor(private productservice : ProductService, private router: Router, private cartService: CartService) {}
+
   ngOnInit(): void {
     this.productservice.getAllProducts().subscribe(
       response=>{ 
          this.products=response.data;
-
       });
   }
+
   viewProductDetails(product:Product):void{
   
     this.router.navigate(['/products', product.id], {
     })
   }
     
-  
+  addProductToCart(product: Product): void{
+    this.cartService.addProductToCart(product);
+  }
 
 }
