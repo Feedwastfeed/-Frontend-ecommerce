95e19beb54dcdba89a76e2f73a9ef8d561bbac7f
AhmedHesham997 <ahmedshafikbeder99@gmail.com>
2023-03-13 22:47:06 +0200

    admin add products

--- src/app/admin/components/manage-products/manage-products.component.ts
+++ src/app/admin/components/manage-products/manage-products.component.ts
@@ -1,8 +1,11 @@
 import { HttpClient } from '@angular/common/http';
-import { Component , OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Category } from 'src/app/models/category';
+import { Product } from 'src/app/models/product';
+import { CategoryService } from 'src/app/services/category/category.service';
 import { ProductService } from 'src/app/services/product/product.service';
-import { Patterns } from 'src/app/shared/patterns/patterns';
+
 
 @Component({
   selector: 'app-manage-products',
@@ -11,43 +14,65 @@ import { Patterns } from 'src/app/shared/patterns/patterns';
 })
 export class ManageProductsComponent implements OnInit {
 
-constructor(private http:HttpClient , private formbuilder:FormBuilder , private productservice:ProductService){}
+  constructor(private http: HttpClient, private formbuilder: FormBuilder, private productservice: ProductService, private categoryservice: CategoryService) { }
 
-productform:FormGroup;
+  productform: FormGroup;
+  ngOnInit(): void {
 
-ngOnInit(): void {
+    this.productform = this.formbuilder.group({
 
-  this.productform=this.formbuilder.group({
+      productname: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(40)]],
+      productdesc: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(250)]],
+      productprice: [1, [Validators.required, Validators.min(1)]],
+      productrate: [1, [Validators.required, Validators.max(5), Validators.min(1)]],
+      productstock: [1, [Validators.required, Validators.min(1), Validators.max(100000), Validators.pattern("^[0-9]*$")]],
+      productcategory: ['', [Validators.required]],
+      productimage: ['', [Validators.required]]
+    });
+    this.categoryservice.getAllCategories().subscribe(
+      response => {
+        this.categories = response.data;
+      })
 
-    productname:['',[Validators.required,Validators.minLength(4),Validators.maxLength(40)]],
-    productdesc:['',[Validators.required,Validators.minLength(5),Validators.maxLength(250)]],
-    productprice:[1,[Validators.required,Validators.min(1)]],
-    productrate:[1,[Validators.required,Validators.max(5),Validators.min(1)]],
-    productstock:[1,[Validators.required,Validators.min(1),Validators.max(100000),Validators.pattern("^[0-9]*$")]],
-    productcategory:['',[Validators.required]],
-    productimage:['',[Validators.required]]
-  });
-      
-}
-
-event:any;
-setEvent(event:any):void{
-this.event=event;
-
-}
-
-addImage():void{
-
-  console.log(event);
-  if(this.event.target.files.length>0){
-    const file =this.event.target.files[0];
-    const formdata = new FormData();
-    formdata.append('file',file);
-
-    this.http.post('http://localhost:9090/ecommerce/products/upload',formdata).subscribe((res:any)=>{
-          
-
-    })
   }
-}
+
+  event: any;
+  categories: Category[];
+  category = new Category();
+  setEvent(event: any): void {
+    this.event = event;
+
   }
+
+  addProduct() {
+    var product = new Product();
+
+    product.name = this.productform.value.productname;
+    product.description = this.productform.value.productdesc;
+    product.price = this.productform.value.productprice;
+    product.rate = this.productform.value.productrate;
+    product.stock = this.productform.value.productstock;
+    let i = 0;
+    while (i < this.categories.length) {
+      if (this.categories[i].name == this.productform.value.productcategory) {
+
+        product.categories.push(this.categories[i]);
+      }
+      i++
+    }
+    product.imagePath = "assets//images//" + this.event.target.files[0].name;
+    this.productservice.add(product);
+
+  }
+
+  addImage(): void {
+    if (this.event.target.files.length > 0) {
+      const file = this.event.target.files[0];
+      const formdata = new FormData();
+      formdata.append('file', file);
+
+      this.productservice.uploadImage(formdata);
+    }
+  }
+
+}
