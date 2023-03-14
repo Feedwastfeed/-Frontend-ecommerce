330cb3067eaf5030b8a307c808129e6ebb948ff9
Ahmed Gamal <ahmed_gamal302@yahoo.com>
2023-03-14 15:19:49 +0200

    Login/Register Impl,AuthService,TokenInterceptor,ErrorInterceptor,Authorization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ResponseViewModel } from './models/responseviewmodel';
 import { CartService } from './services/cart/cart.service';
+import { AuthService } from './services/auth/auth.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,15 +11,28 @@ import { CartService } from './services/cart/cart.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+  cart = 'bi bi-cart-check';
   title = 'ecommerce';
 
-  constructor(private cartServices: CartService){}
+  constructor(private cartServices: CartService,public authService:AuthService,private router:Router){}
  
 
  countCart():number{
-  console.log(this.cartServices.countProductCart());
+  // console.log(this.cartServices.countProductCart());
+  if (this.cartServices.countProductCart()>0) {
+    this.cart='bi bi-cart-check-fill';
+  }
   return this.cartServices.countProductCart();
  }
 
+ logout(){
+  console.log(this.authService.isLoggedIn(),this.authService.isCustomer(),this.authService.isAdmin(),this.authService.getCurrentUserId());
+  this.authService.userLogout();
+  console.log(this.authService.isLoggedIn());
+  
+  this.router.navigate(['/login'])
+ }
 }
--- src/app/services/auth/token-interceptor.service.ts
+++ src/app/services/auth/token-interceptor.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable, Injector } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TokenInterceptorService {
+export class TokenInterceptorService{
 
-  constructor() { }
+  
+  constructor(private injector:Injector) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler){
+    var tokenReq = req.clone();
+    const authService = this.injector.get(AuthService);
+    
+    if(authService.isLoggedIn()==true){
+     tokenReq = req.clone({
+      setHeaders:
+      {
+        Authorization: `Bearer ${authService.getToken()}`
+      }
+    })
+       }
+       return next.handle(tokenReq);
+ 
+
+}
+
 }
